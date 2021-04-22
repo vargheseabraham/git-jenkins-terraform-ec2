ccc6e90d5a872860edf91d6a6ab7b53d16174b0b
vargheseabraham <40785256+vargheseabraham@users.noreply.github.com>
2021-04-22 20:26:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-03d64741867e7bb94"
+  instance_type = "t2.micro"
+  key_name      = "TerraformKeyPair"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
